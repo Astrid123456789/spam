7ff13233ca5a8b46e7e4c7bd94a4790e16d6ef1b
Meryem <bendahmanmeryem1@gmail.com>
2025-12-05 10:49:30 +0100

    feat: add evaluator

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -4,3 +4,7 @@ from src.pipeline.evaluator import Evaluator
 from src.utils.config import POSITIVE_CLASS_LABEL
 
 class TestEvaluator:
+    @pytest.fixture
+    def evaluator(self):
+        """Fixture to initialize the Evaluator."""
+        return Evaluator()
