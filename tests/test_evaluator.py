145981b876f51cd28387e0458caa6b84ef15a4f2
Meryem <bendahmanmeryem1@gmail.com>
2025-12-05 10:51:28 +0100

    feat:add perfect predictions

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -8,3 +8,9 @@ class TestEvaluator:
     def evaluator(self):
         """Fixture to initialize the Evaluator."""
         return Evaluator()
+    @pytest.fixture
+    def perfect_predictions(self):
+        """Case where predictions match truth exactly."""
+        y_true = np.array([0, 1, 0, 1, 1])
+        y_pred = np.array([0, 1, 0, 1, 1])
+        return y_true, y_pred
